57ff391abbfad4571a809415157978b806b36608
troy-gilman <troygilman@gmail.com>
2026-09-23 13:35:49 -0400

    Update draggable.js

--- draggable/draggable.js
+++ draggable/draggable.js
@@ -5,6 +5,10 @@ const styles = /* css */ `
     border: 1px solid black;
     width: fit-content;
     height: fit-content;
+    transition: box-shadow 0.3s ease;
+}
+.sb-draggable-container-targetted {
+    box-shadow: 0 0 20px #0ff, 0 0 40px #0ff;
 }
 .sb-draggable-trigger {
     cursor: pointer;
@@ -23,9 +27,15 @@ rocket("sb-draggable-root", {
     `,
 });
 
+function isPointInElement(el, x, y) {
+  const r = el.getBoundingClientRect();
+  return x >= r.left && x <= r.right && y >= r.top && y <= r.bottom;
+}
+
 rocket("sb-draggable-container", {
     setup: ({ $$, action, effect, adoptStyles, host, emit }) => {
         $$.dragging = false;
+        $$.targetted = false;
         $$.originalX = 0;
         $$.originalY = 0;
         $$.offsetX = 0;
@@ -37,22 +47,27 @@ rocket("sb-draggable-container", {
             $$.originalY = y;
             console.log("grabbed", $$.originalX, $$.originalY);
         });
-        action("released", () => {
+        action("released", ({ el }, {x, y}) => {
             $$.dragging = false;
             $$.offsetX = 0;
             $$.offsetY = 0;
+            emit("dragged", {id: el.id, x: x, y: y, released: true})
             console.log("released");
+
         });
-        action("mousemove", (_, { x, y }) => {
+        action("mousemove", ({ el }, { x, y }) => {
             if ($$.dragging) {
                 $$.offsetX = x - $$.originalX;
                 $$.offsetY = y - $$.originalY;
-                emit("dragged", {id: host.id, x: x, y: y})
+                emit("dragged", {id: el.id, x: x, y: y, released: false})
             }
         });
-        action("dragged", (_, { id, x, y }) => {
-            if (host.id !== id) {
-                console.log("dragged", id, x, y)
+        action("dragged", ({ el }, { id, x, y, released }) => {
+            if (el.id !== id && isPointInElement(el, x, y) && !released) {
+                console.log("hover", el.id, x, y);
+                $$.targetted = true;
+            } else {
+                $$.targetted = false;
             }
         })
         effect(() => {
@@ -60,14 +75,15 @@ rocket("sb-draggable-container", {
         });
         adoptStyles(host, styles)
     },
-    render: ({ html, props: {} }) => html`
-        <div
+    render: ({ html, host }) => html`
+        <div id="sb-draggable-container-${host.id}"
             class="sb-draggable-container"
             data-on:grabbed="@grabbed(evt.detail)"
-            data-on:released="@released()"
+            data-on:released="@released(evt.detail)"
             data-on:mousemove__window="@mousemove({x: evt.clientX, y: evt.clientY})"
             data-on:dragged__window="@dragged(evt.detail)"
             data-style:translate="$$translate"
+            data-class:sb-draggable-container-targetted="$$targetted"
         >
             <slot></slot>
         </div>
@@ -82,10 +98,10 @@ rocket("sb-draggable-trigger", {
             $$.grabbed = true;
             emit("grabbed", { x: x, y: y });
         });
-        action("mouseup", () => {
+        action("mouseup", (_, {x, y}) => {
             if ($$.grabbed) {
                 $$.grabbed = false;
-                emit("released");
+                emit("released", {x: x, y: y});
             }
         });
         adoptStyles(host, styles);
@@ -94,7 +110,7 @@ rocket("sb-draggable-trigger", {
         <div
             class="sb-draggable-trigger"
             data-on:mousedown="@mousedown({x: evt.clientX, y: evt.clientY})"
-            data-on:mouseup__window="@mouseup()"
+            data-on:mouseup__window="@mouseup({x: evt.clientX, y: evt.clientY})"
         >
             <slot></slot>
         </div>
